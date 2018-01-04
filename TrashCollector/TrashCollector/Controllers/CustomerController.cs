adadd6fd40d06096c38392eef75514ac9a954d67
David Merkel <merkel_david@yahoo.com>
2018-01-04 15:23:46 -0600

    added customer home screen

--- TrashCollector/TrashCollector/Controllers/CustomerController.cs
+++ TrashCollector/TrashCollector/Controllers/CustomerController.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
     public class CustomerController : Controller
     {
+
+        ApplicationDbContext context;
         // GET: Customer
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditAddress()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAddress(AddressViewModel model)
+        {
+            return View();
+        }
+
+        public ActionResult MakePayments()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> MakePayments(PaymentViewModel model)
+        {
+            return View();
+        }
+
+        public ActionResult ChangeSchedule()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangeSchedule(ScheduleViewModel model)
         {
             return View();
         }
---
+++ TrashCollector/TrashCollector/Models/CustomerViewModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class AddressViewModel
+    {
+        [Display(Name = "Address")]
+        public Adress Adress { get; set; }
+    }
+
+    public class PaymentViewModel
+    {
+        public int PaymentID { get; set; }
+        [Required]
+        public int CardNumber { get; set; }
+        [Required]
+        public DateTime ExpirationDate { get; set; }
+        [Required]
+        public int SecurityCode { get; set; }
+    }
+
+
+    public class ScheduleViewModel
+    {
+
+    }
+}
